b5fc9b57a2b1ed3f0e5d36493b6b2aba03eba89e
davi <daavisouza.santana@gmail.com>
2023-01-25 17:21:38 -0300

    fix: fixingAutoLogin

--- src/Pages/RegisterPage/index.tsx
+++ src/Pages/RegisterPage/index.tsx
@@ -1,12 +1,17 @@
 import { DefaultBanner } from "../../components/DefaultBanner"
 import { RegisterBox } from "../../components/RegisterBox"
 import { StyledRegisterPage } from "./style"
+import { Navigate } from "react-router-dom";
 
 export function RegisterPage () {
-    return (
+
+    const token = localStorage.getItem("@TOKEN");
+
+    return token === null ?
         <StyledRegisterPage className="display-flex justify-content-center align-item gap-60 column-mobile page-mobile gap-14-moblie">
             <DefaultBanner/>
             <RegisterBox/>
         </StyledRegisterPage>
-    )
+    :
+    <Navigate to={"/dashboard"}/>
 }
